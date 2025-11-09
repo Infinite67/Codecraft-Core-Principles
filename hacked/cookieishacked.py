d4b061c8bf39a0ddb80649c25ca2978b4d594930
Cookie  <cookiexlks@gmail.com>
2025-11-09 14:54:53 -0500

    Create cookieishacked.py

---
+++ hacked/cookieishacked.py
@@ -0,0 +1,23 @@
+<h1 allign="center">Cookie was hacked hehe :)/<h1 
+
+alert("cookiexlks is not getting away with this one >:)".);
+
+function(_)0x_736838929387463
+
+[if.length.go] 
+
+c=import("Well, you were caught :/.");
+
+numbers = 94048475634
+
+GETGENV
+               {
+
+
+ cookie's password: cookiexlksisthebest123
+
+say("NOOOOOO, WHYYYY 💀 R.I.P..");
+
+
+"username": "testuser"
+"password": "StrongP@ssw0rd"
